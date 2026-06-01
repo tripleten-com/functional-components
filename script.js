6e8e85a6e768ac09704273ced08960feb997d787
tripleten-student <87186064+tripleten-student@users.noreply.github.com>
2026-06-01 12:27:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+ReactDOM.render(
+  (<>
+    <div>
+      <h2>Alex</h2>
+      <p>Web Developer</p>
+    </div>
+    <div>
+      <h2>Lara</h2>
+      <p>Scientist</p>
+    </div>
+  </>),
+  document.querySelector('#root'),
+);
